3cdca4e035e6c8512efa3f131488bfa6fb48af1c
kardinal-eros <roli@lore.fritz.box>
2014-03-16 11:42:52 +0100

    added row and col arguments to replaceTaxon

--- pkg/R/extractTaxon.R
+++ pkg/R/extractTaxon.R
@@ -1,12 +1,11 @@
-extractTaxon <- function (x, n, blank = "blank") {
-	if (missing(n))
+extractTaxon <- function (x, col, row, blank = "blank") {
+	if (missing(col) | missing(row))
 		stop ("need starting row and width of chars for taxa block")
-	stopifnot(length(n) == 2)
 	
 	stopifnot(inherits(x, "character"))	
 
-	x <- sapply(x, function (x) substring(x, 1, n[2]), USE.NAMES = FALSE)
-	x <- x[n[1]:(length(x))]
+	x <- sapply(x, function (x) substring(x, 1, col), USE.NAMES = FALSE)
+	x <- x[row:(length(x))]
 	x <- str_trim(x)
 	x[x == ""] <- blank
 	if (any(is.na(x)))
