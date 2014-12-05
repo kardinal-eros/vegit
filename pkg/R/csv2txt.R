f87b10ce24e90f2cbc474e38ee88ff0496fbc943
kardinal-eros <roli@Zora.local>
2014-12-05 20:18:39 +0100

    added arguments to csv2txt and …
    … performed tests

--- pkg/R/csv2txt.R
+++ pkg/R/csv2txt.R
@@ -1,4 +1,4 @@
-csv2txt <- function (x, header.rows, merge.cols, sep = ";", overwrite = FALSE, ...) {
+csv2txt <- function (x, header.rows, merge.cols, sep = ";", width = 1, vertical = TRUE, collapse = " ", overwrite = FALSE, ...) {
 	if (missing(header.rows))
 		stop("please define the number of header rows", call. = FALSE)
 	else
@@ -8,6 +8,9 @@ csv2txt <- function (x, header.rows, merge.cols, sep = ";", overwrite = FALSE, .
 		j <- as.vector(merge.cols)
 	else
 		j <- 1
+	
+	if (!vertical & missing(width))
+		stop("must define with for verital = FALSE")
 		
 	#	output filename
 	f <- gsub(".csv", ".txt", x, fixed = TRUE)
@@ -27,39 +30,53 @@ csv2txt <- function (x, header.rows, merge.cols, sep = ";", overwrite = FALSE, .
 	#	transform header
 	#	format to equal width, add leading zeros (numbers)
 	#	or trailing dots (character strings)
-	l <- as.matrix(x)[1:n, 1, drop = FALSE ]     # labels	
+	l <- as.matrix(x)[1:n,  1, drop = FALSE ] # labels	
 	v <- as.matrix(x)[1:n, -1, drop = FALSE ] # values
-	
-	#	width of strings (w) and type of value (t)
-	if (nrow(v) > 1) {
-		w <- apply(apply(v, 2, function (x) sapply(x, nchar)), 1, max)
-		t <- apply(v, 1, function (x) class(type.convert(x)))
-	} else {
-		w <- apply(v, 2, nchar)
-		t <- sapply(v, function (x) class(type.convert(x)))		
+		
+	if (vertical) {
+		#	width of strings (w) and type of value (t)
+		if (nrow(v) > 1) {
+			w <- apply(apply(v, 2, function (x) sapply(x, nchar)), 1, max)
+			t <- apply(v, 1, function (x) class(type.convert(x)))
+		} else {
+			w <- apply(v, 2, nchar)
+			t <- sapply(v, function (x) class(type.convert(x)))		
+		}
+		for (i in seq_along(l)) {
+			if (t[i] == "integer")
+				v[i, ] <- sprintf(paste0("%0", w[i], "d"), as.integer(v[i, ]))	
+			else
+				v[i, ] <- gsub("[[:space:]]", ".", sprintf(paste0("%", w[i], "s"), v[i,]))
+		}
+
+		#	rbind
+		m <- c()
+		for (i in 1:nrow(v)) {
+			ii <- unlist(sapply(v[i, ], strsplit, ""))
+			m <- rbind(m, matrix(ii, nrow = w[i], ncol = ncol(v)))
+		}
+		
+		#	assign labels
+		if (n > 1) {
+			m <- cbind(rep.int(l, times = w), m)
+			m[duplicated(m[,1]), 1] <- ""
+		} else {
+			m <- cbind(l, m)
+		}
 	}
 	
-	for (i in seq_along(l)) {
-		if (t[i] == "integer")
-			v[i, ] <- sprintf(paste0("%0", w[i], "d"), as.integer(v[i, ]))	
-		else
-			v[i, ] <- gsub("[[:space:]]", ".", sprintf(paste0("%-", w[i], "s"), v[i,]))
-	}
-	
-	#	for ease use rbind
-	m <- c()
-	for (i in 1:nrow(v)) {
-		ii <- unlist(sapply(v[i, ], strsplit, ""))
-		m <- rbind(m, matrix(ii, nrow = w[i], ncol = ncol(v)))
-	}
-	
-	#	assign labels
-	if (n > 1) {
-		m <- cbind(rep.int(l, times = w), m)
-		m[duplicated(m[,1]),1] <- ""
-	} else {
+	if (!vertical) {	
+		for (i in 1:ncol(v)) {
+			#	%s right justyfied
+			#	%-s left justyfied
+			v[, i] <- sprintf(paste0("%", width, "s"), v[, i])
+			m <- v
+			dimnames(m) <- NULL
+		}
+		#	assign labels		
 		m <- cbind(l, m)
 	}
+				
 	
 	#	taxa block
 	#	there may be empty rows seperating blocks?
@@ -68,20 +85,26 @@ csv2txt <- function (x, header.rows, merge.cols, sep = ";", overwrite = FALSE, .
 	v[v == ""] <- "."
 	v[b, ] <- ""
 
-	#	combine with header, result (m) to be returned by function
-	m <- rbind(m, v)
-	m <- t <- as.data.frame(m, stringsAsFactors = FALSE, row.names = 1:nrow(m))
+	#	combine with header, result (r) to be returned by function
+	r <- rbind(m, v)
+	r <- t <- as.data.frame(r, stringsAsFactors = FALSE, row.names = 1:nrow(m))
 	
 	#	transform to text
 	
 	#	format width of first column
 	t[, 1] <- sprintf(paste0("%-", max(sapply(x, nchar)) + 1, "s"), t[, 1])
-	#	as text
-	t <- apply(t, 1, function (x) paste0(unlist(strsplit(x, ""))))
-	t <- sapply(t, paste0, collapse = "")
+
+	#	format width of remaining columns
+	for (i in 2:ncol(t)) {
+		t[, i] <- sprintf(paste0("%", width, "s"), t[, i])
+	}
+		
+	#	save matrix as text
+	t <- apply(as.matrix(t), 1, paste0, collapse = "") # arggument collapse 
 	con <- file(f)
 	writeLines(t, con)
 	close(con)
 
-	return(m)
+	#	return matrix
+	return(r)
 }
