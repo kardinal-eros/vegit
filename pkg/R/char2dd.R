545c1900774f8b7824e48db3161aec074de99e67
Roland Kaiser <kardinal.eros@gmail.com>
2015-01-21 12:32:04 +0100

    fixed bug related to decimal seperator

--- pkg/R/char2dd.R
+++ pkg/R/char2dd.R
@@ -3,6 +3,8 @@
 char2dd <- function (x, chd = "\u00B0", chm = "\"", chs = "\"\"", NS = TRUE) {
 	#	convert to default of char2dms in package sp
 	#	gsub will instantly convert to character if it is not already
+	x <- gsub(",", ".", x, fixed = TRUE) # decimals
+	
 	x <- gsub(chd, "d", x, fixed = TRUE)
 	x <- gsub(chm, "'", x, fixed = TRUE)
 	
@@ -10,13 +12,14 @@ char2dd <- function (x, chd = "\u00B0", chm = "\"", chs = "\"\"", NS = TRUE) {
 	n <- nchar(chs)
 	nn <- unlist(strsplit(chs, "*"))
 	if (n > 1 & unique(nn) == chm)	
-		x <- gsub(paste0(rep(chm, n), collapse = ""), "\"", x, fixed = TRUE)
+		x <- gsub(paste0(rep("'", n), collapse = ""), "\"", x, fixed = TRUE)
 	else
 		x <- gsub(chs, "\"", x, fixed = TRUE)	
 		
 	i <- sapply(sapply(x, type.convert, simplify = FALSE), is.numeric)
 	x1 <- unlist(x)[i]
 	x2 <- unlist(x)[!i]
+	
 	x2 <- paste0(x2, ifelse(NS, "N", "E"))
 	x2 <- char2dms(x2)
 	
