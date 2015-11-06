e7393807357df6a28feeb80fdfe3a0af79accac6
Roland Kaiser <kardinal.eros@gmail.com>
2015-11-06 09:24:44 +0100

    improved header alignment

--- pkg/R/csv2txt.R
+++ pkg/R/csv2txt.R
@@ -1,3 +1,56 @@
+.vertical <- function (x) {
+	#	format to equal width, add leading zeros (numbers)
+	#	or trailing dots (character strings)
+
+	stopifnot(mode(x) == "character")
+	l <- x[, 1]
+	w <- apply(t(apply(x[, -1, drop = FALSE], 1, nchar)), 1, max)
+	v <- x[, -1, drop = FALSE]
+	
+	#	width of strings (w) and type of value (t)
+	if (nrow(v) > 1) {
+		w <- apply(apply(v, 2, function (x) sapply(x, nchar)), 1, max)
+	} else {
+		w <- max(apply(v, 2, nchar))
+	}
+	t <- apply(v, 1, function (x) class(type.convert(x)))
+	
+	for (i in seq_along(l)) {
+		if (t[i] == "integer") {
+			v[i, ] <- sprintf(paste0("%0", w[i], "d"), as.integer(v[i, ]))
+		} else {
+			if (t[i] == "numeric") {
+				v[i, ] <- gsub("[[:space:]]", ".", sprintf(paste0("%", w[i], "s"), v[i,]))	
+			} else {
+				v[i, ] <- gsub("[[:space:]]", ".", sprintf(paste0("%", w[i], "-s"), v[i,]))
+			}
+		}	
+	}
+	#	rbind
+	m <- c()
+	for (i in 1:nrow(v)) {
+		ii <- unlist(sapply(v[i, ], strsplit, ""))
+		m <- rbind(m, matrix(ii, nrow = w[i], ncol = ncol(v)))
+	}
+	
+	#	assign labels
+	m <- cbind(rep.int(l, times = w), m)
+	m[duplicated(m[,1]), 1] <- ""
+	
+	return(m)
+}
+
+#	example data
+#	x <- t(data.frame(plot = 9:12,
+#		value1 = round(abs(rnorm(4)), 1),
+#		value2 = round(rnorm(4), 1),
+#		value3 = ceiling(abs(rnorm(4) * 10)),		
+#		value4 = sample(1:100, 4),
+#		value5 = c("abcd", "b", "bc", "acd")))
+#	x <- cbind(rownames(x), x)
+#	dimnames(x) <- NULL
+#	x <- gsub(" ", "", x)
+
 csv2txt <- function (x, header.rows, merge.cols, sep = ";", width = 1, vertical = TRUE, collapse = " ", overwrite = FALSE, ...) {
 	if (missing(header.rows))
 		stop("please define the number of header rows", call. = FALSE)
@@ -19,76 +72,48 @@ csv2txt <- function (x, header.rows, merge.cols, sep = ";", width = 1, vertical 
 			"\nuse overwrite = TRUE", call. = FALSE)
 	
 	#	read file
-	x <- read.csv(x, sep = sep, colClasses = "character", header = FALSE)#, ...)
+	x <- read.csv(x, sep = sep, colClasses = "character", header = FALSE)
 	
-	if (length(j) > 1) {
-		x1 <- apply(x[, j], 1, function (x) paste(x, collapse = " "))
-		x2 <- x[ ,-j]
+	if (j > 1) {
+		x1 <- apply(x[ , 1:j ], 1, function (x) paste(x, collapse = "@"))
+		x2 <- x[ ,-(1:j) ]
 		x <- as.matrix(cbind(x1, x2))
 	}
 	
-	#	transform header
-	#	format to equal width, add leading zeros (numbers)
-	#	or trailing dots (character strings)
-	l <- as.matrix(x)[1:n,  1, drop = FALSE ] # labels	
-	v <- as.matrix(x)[1:n, -1, drop = FALSE ] # values
-		
-	if (vertical) {
-		#	width of strings (w) and type of value (t)
-		if (nrow(v) > 1) {
-			w <- apply(apply(v, 2, function (x) sapply(x, nchar)), 1, max)
-			t <- apply(v, 1, function (x) class(type.convert(x)))
-		} else {
-			w <- apply(v, 2, nchar)
-			t <- sapply(v, function (x) class(type.convert(x)))		
+	#	transform header or plat identifierif present
+		if (vertical) {
+			m <- .vertical(x[ 1, , drop = FALSE])
 		}
-		for (i in seq_along(l)) {
-			if (t[i] == "integer")
-				v[i, ] <- sprintf(paste0("%0", w[i], "d"), as.integer(v[i, ]))	
-			else
-				v[i, ] <- gsub("[[:space:]]", ".", sprintf(paste0("%", w[i], "s"), v[i,]))
-		}
-
-		#	rbind
-		m <- c()
-		for (i in 1:nrow(v)) {
-			ii <- unlist(sapply(v[i, ], strsplit, ""))
-			m <- rbind(m, matrix(ii, nrow = w[i], ncol = ncol(v)))
-		}
-		
-		#	assign labels
-		if (n > 1) {
-			m <- cbind(rep.int(l, times = w), m)
-			m[duplicated(m[,1]), 1] <- ""
-		} else {
+		if (!vertical) {	
+			for (i in 1:ncol(v)) {
+				#	%s right justyfied
+				#	%-s left justyfied
+				v[, i] <- sprintf(paste0("%", width, "s"), v[, i])
+				m <- v
+				dimnames(m) <- NULL
+			}
+			#	assign labels		
 			m <- cbind(l, m)
-		}
-	}
-	
-	if (!vertical) {	
-		for (i in 1:ncol(v)) {
-			#	%s right justyfied
-			#	%-s left justyfied
-			v[, i] <- sprintf(paste0("%", width, "s"), v[, i])
-			m <- v
-			dimnames(m) <- NULL
-		}
-		#	assign labels		
-		m <- cbind(l, m)
-	}
-				
+		}			
 	
 	#	taxa block
+	#	with or without header
+	if (n > 0) {
+		v <- as.matrix(x[-c(1:n), ])
+	} else {
+		v <- as.matrix(x[ -1, ]) # read.csv(..., header = FALSE)
+		
+	}	
 	#	there may be empty rows seperating blocks?
-	v <- as.matrix(x[-c(1:n), ])
 	b <- v[,1] == ""	
 	v[v == ""] <- "."
 	v[b, ] <- ""
 
-	#	combine with header, result (r) to be returned by function
+	#	combine with header if present, result (r) to be returned by function
+	if (n > 0) {
 	r <- rbind(m, v)
 	r <- t <- as.data.frame(r, stringsAsFactors = FALSE, row.names = 1:nrow(m))
-	
+	}		
 	#	transform to text
 	
 	#	format width of first column
@@ -100,7 +125,7 @@ csv2txt <- function (x, header.rows, merge.cols, sep = ";", width = 1, vertical 
 	}
 		
 	#	save matrix as text
-	t <- apply(as.matrix(t), 1, paste0, collapse = "") # arggument collapse 
+	t <- apply(as.matrix(t), 1, paste0, collapse = "") # argument collapse?
 	con <- file(f)
 	writeLines(t, con)
 	close(con)
